59c747b3a56756f24e147b0606742404a984b01d
Muhammad Alif Fauzan <fauzanalief40@gmail.com>
2026-07-04 06:54:13 +0700

    Test failed pipeline

--- app/test_app.py
+++ app/test_app.py
@@ -6,7 +6,7 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.data == b"Hello DevOps!"
+    assert response.data == b"Hello Salah!"
 
 
 def test_health():
