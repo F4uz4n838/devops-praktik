4cdef37898a8887e41286edb0b347d8977142403
Muhammad Alif Fauzan <fauzanalief40@gmail.com>
2026-07-04 07:04:38 +0700

    Fix unit test

--- app/test_app.py
+++ app/test_app.py
@@ -6,7 +6,7 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.data == b"Hello Salah!"
+    assert response.data == b"Hello DevOps!"
 
 
 def test_health():
